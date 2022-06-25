61846f16bdae3c64897f5f2f418e8739edb17e7a
Alexander Berryhill <amsb99@gmail.com>
2022-06-25 13:05:39 -0600

    Update getData.js

--- js/getData.js
+++ js/getData.js
@@ -1,17 +1,17 @@
 // import fetch from 'electron-fetch'
 // or
-// const fetch = require('electron-fetch').default
+const fetch = require('electron-fetch').default
 
 // plain text or html
 
 appointments = fetch('https://momsstore.herokuapp.com/customer')
-	.then(res => console.log(res))
-	// .then(body => console.log(body))
+	.then(res => res.text())
+	.then(body => console.log(body))
 
 console.log(appointments)
-// var appointment_div = []
-// //For every appointment put the student into the sidebar
-// for(var i = 0; i < appointments.length; i++){
-// 	appointment_div += "<div class='appointment'>"
-// }
-// document.getElementById('')
+var appointment_div = []
+//For every appointment put the student into the sidebar
+for(var i = 0; i < appointments.length; i++){
+	appointment_div += "<div class='appointment'>"+appointments[i].name+
+}
+document.getElementById('name_container').innerHTML=appointment_div;
