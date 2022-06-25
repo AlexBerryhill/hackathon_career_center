80681f986f95d7357ef14a4f075192347f3a677b
Alexander Berryhill <amsb99@gmail.com>
2022-06-25 15:21:23 -0600

    Update from main

--- js/getData.js
+++ js/getData.js
@@ -1,14 +1,15 @@
 <<<<<<< HEAD
 // import fetch from 'electron-fetch'
 // or
-const fetch = require('electron-fetch').default
+// const fetch = require('electron-fetch').default
 
 // plain text or html
 
-appointments = fetch('https://momsstore.herokuapp.com/customer')
-	.then(res => res.text())
-	.then(body => console.log(body))
+// appointments = fetch('https://momsstore.herokuapp.com/customer')
+// 	.then(res => res.text())
+// 	.then(body => console.log(body))
 
+<<<<<<< Updated upstream
 console.log(appointments)
 var appointment_div = []
 //For every appointment put the student into the sidebar
@@ -25,3 +26,15 @@ fetch("https://momsstore.herokuapp.com/customer")
     console.log(data);
   });
 >>>>>>> main
+=======
+function populateNames(){
+	appointments = [{name: Alex}, {name: Denis}, {name: Michael}, {name: Aiden}]
+	console.log(appointments)
+	var appointment_div = []
+	//For every appointment put the student into the sidebar
+	for(var i = 0; i < appointments.length; i++){
+		appointment_div += "<div class='appointment'>"+appointments[i].name+"</div>"
+	}
+	document.getElementById('name_container').innerHTML=appointment_div;
+}
+>>>>>>> Stashed changes
