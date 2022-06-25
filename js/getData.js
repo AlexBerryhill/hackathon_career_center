220c0b6bb398cf6f885b00b1f08a1122c79150a9
Denis Lazo <denislazo1610@gmail.com>
2022-06-25 14:36:55 -0600

    fetching data

--- js/getData.js
+++ js/getData.js
@@ -1,17 +1,7 @@
-// import fetch from 'electron-fetch'
-// or
-// const fetch = require('electron-fetch').default
-
-// plain text or html
-
-appointments = fetch('https://momsstore.herokuapp.com/customer')
-	.then(res => console.log(res))
-	// .then(body => console.log(body))
-
-console.log(appointments)
-// var appointment_div = []
-// //For every appointment put the student into the sidebar
-// for(var i = 0; i < appointments.length; i++){
-// 	appointment_div += "<div class='appointment'>"
-// }
-// document.getElementById('')
+fetch("https://momsstore.herokuapp.com/customer")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
