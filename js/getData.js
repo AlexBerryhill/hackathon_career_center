b0621aa5d6bcbac38480ba9b0f81506d3b765cb3
Alexander Berryhill <amsb99@gmail.com>
2022-06-25 19:52:30 -0600

    Added names rows

--- js/getData.js
+++ js/getData.js
@@ -1,34 +1,30 @@
 // import fetch from 'electron-fetch'
 // or
 // const fetch = require('electron-fetch').default
+// var nodemailer = require("nodemailer");
 
 // plain text or html
 
 // appointments = fetch('https://momsstore.herokuapp.com/customer')
 // 	.then(res => res.text())
 // 	.then(body => console.log(body))
+async function getNames(url){
+	return await fetch(url)
+	.then((response) => {
+		return response.json()
+	})
+}
 
-fetch("https://momsstore.herokuapp.com/customer")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
-
-function populateNames(){
-	appointments = [{name: Alex}, {name: Denis}, {name: Michael}, {name: Aiden}]
+async function populateNames(){
+	console.log("'"+String(document.getElementById('url').value)+"'")
+	var appointments = await getNames(document.getElementById('url').value)
 	console.log(appointments)
 	var appointment_div = []
 	//For every appointment put the student into the sidebar
 	for(var i = 0; i < appointments.length; i++){
-		appointment_div += "<div class='appointment'>"+appointments[i].name+"</div>"
+		appointment_div += "<div class='appointment'>"+appointments[i].FirstName+"</div>"
 	}
+	console.log(appointment_div)
 	document.getElementById('name_container').innerHTML=appointment_div;
 }
 
-function init(){
-	document.getElementById("submit").addEventListener('click', function() {
-    	populateNames();
-	});
-}
--- main.js
+++ main.js
@@ -1,55 +1,89 @@
-const {app, BrowserWindow} = require('electron')
-  const path = require('path')
-  const url = require('url')
-  
-  // Keep a global reference of the window object, if you don't, the window will
-  // be closed automatically when the JavaScript object is garbage collected.
-  let win
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path')
+const url = require('url')
+const nodemailer = require("nodemailer");
 
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
-  
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
-  
-    // Open the DevTools.
-    // win.webContents.openDevTools()
-  
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      win = null
-    })
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
   
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow)
-  
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit()
+function SendIt() {
+  var transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    tls: {
+       ciphers:'SSLv3'
+    },
+    auth: {
+        user: 'mymail@outlook.com',
+        pass: 'myPassword'
     }
-  })
+  });
   
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow()
-    }
-  })
+  const mailOptions = {
+    from: '"Our Code World " <mymail@outlook.com>', // sender address (who sends)
+    to: 'mymail@mail.com, mymail2@mail.com', // list of receivers (who receives)
+    subject: 'Hello ', // Subject line
+    text: 'Hello world ', // plaintext body
+    html: '<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
+  };
   
-  // In this file you can include the rest of your app's specific main process
-  // code. You can also put them in separate files and require them here.
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+}
+    
+ipcMain.on("SendIt", (event, args) => {
+  console.log("ipcMain: Executing SendIt");
+  SendIt();
+});
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
