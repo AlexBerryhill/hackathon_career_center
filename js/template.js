ad189f261024bbeb9e2802db140458a461503703
Denis Lazo <denislazo1610@gmail.com>
2022-06-27 17:02:54 -0600

    some changes

--- js/template.js
+++ js/template.js
@@ -1,26 +1,26 @@
-const path = require('path')
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-function fillTemplateCards(){
-    //joining path of directory 
-    const directoryPath = path.join(__dirname, 'templates');
-    var template_cards = ''
-    //passsing directoryPath and callback function
-    fs.readdir(directoryPath, function (err, files) {
-        //handling error
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-        //listing all files using forEach
-        files.forEach(function (file) {
-            template_cards += "<div class = 'template_card'>" +file+"</div>";
-        });
-        document.getElementById('template_cards_container').innerHTML=template_cards
+function fillTemplateCards() {
+  //joining path of directory
+  const directoryPath = path.join(__dirname, "templates");
+  var template_cards = "";
+  //passsing directoryPath and callback function
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+      template_cards += "<div class = 'template_card'>" + file + "</div>";
     });
+    document.getElementById("template_cards_container").innerHTML =
+      template_cards;
+  });
 }
 
-function fillTemplate(template){
-    const directoryPath = path.join(__dirname, 'templates');
-
+function fillTemplate(template) {
+  const directoryPath = path.join(__dirname, "templates");
 }
-class 
+// class
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const url = require("url");
-const { sendIt } = require("./js/sendEmail");
+// const { sendIt } = require("./js/sendEmail");
 
-sendIt();
+// sendIt();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
