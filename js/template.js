786f8ce76d1bc2a4c3f7d4545f6627c37bf1ecfc
Alexander Berryhill <amsb99@gmail.com>
2022-06-27 17:40:28 -0600

    fill templates is working

--- js/template.js
+++ js/template.js
@@ -13,14 +13,25 @@ function fillTemplateCards(){
         } 
         //listing all files using forEach
         files.forEach(function (file) {
-            template_cards += "<div class = 'template_card'>" +file+"</div>";
+            template_cards += "<div class = 'template_card' onclick='fillTemplate("+'"'+file+'"'+", "+'"Alex",'+ '"4:20pm",'+ '"Denis"'+")'>" +file+"</div>";
         });
+        console.log(template_cards)
         document.getElementById('template_cards_container').innerHTML=template_cards
     });
 }
 
-function fillTemplate(template){
-    const directoryPath = path.join(__dirname, 'templates');
+function fillTemplate(template_name, name, time, your_name){
+    var client = new XMLHttpRequest();
+    client.open('GET', './templates/'+template_name);
+    client.onreadystatechange = function() {
+        const directoryPath = path.join(__dirname, 'templates');
 
+        //Get Template from folder
+        alert(client.responseText);
+        var template = client.responseText
+        
+        template = template.replace('{name}', name).replace('{time}', time).replace('{your_name}', your_name)
+        document.getElementById('template_cards_container').innerHTML=template
+    }
+    client.send();
 }
-class 
