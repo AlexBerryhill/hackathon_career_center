2cd25f7e43a7f1253b6420960aa30f77520cba3d
Alexander Berryhill <amsb99@gmail.com>
2022-06-27 20:22:24 -0600

    create Template works

--- js/template.js
+++ js/template.js
@@ -15,7 +15,6 @@ function fillTemplateCards(){
         files.forEach(function (file) {
             template_cards += "<div class = 'template_card' onclick='fillTemplate("+'"'+file+'"'+", "+'"Alex",'+ '"4:20pm",'+ '"Denis"'+")'>" +file+"</div>";
         });
-        console.log(template_cards)
         document.getElementById('template_cards_container').innerHTML=template_cards
     });
 }
@@ -37,8 +36,8 @@ function fillTemplate(template_name, name, time, your_name){
 
 function openTemplateCreater(){
     html =  "<div>"+
-            "    <input type = 'text' name = 'template' placeholder='Title'></input>"+
-            "    <textarea name='template_text' id='template_text' cols='30' rows='10'></textarea>"+
+            "    <input type = 'text' name = 'template_title' id = 'template_title' placeholder='Title'></input>"+
+            "    <textarea name='template_text' id='template_text' cols='60' rows='20' placeholder='content'></textarea>"+
             "    <button onclick='createTemplate()'>Create</button>"+
             "</div>"
     document.getElementById('template_cards_container').innerHTML=html
@@ -48,19 +47,40 @@ function createTemplate(){
     title = document.getElementById('template_title').value;
     text = document.getElementById('template_text').value;
     html =  '<!DOCTYPE html>'+
-            '    <html>'+
-            '    <head>'+
-            '        <base target="_top">'+
-            '    </head>'+
-            '    <body>'+
-            '        <p>'+
+            '\n    <html>'+
+            '\n    <head>'+
+            '\n        <base target="_top">'+
+            '\n    </head>'+
+            '\n    <body>'+
+            '\n        <p>'+
                         text+
-            '           <br>{your_name}<br>'+
-            '           Career Success Mentor<br>'+
-            '           BYU-I Career Center | MC 200 <br>'+ 
-            '           208.496.9825  <br>'+
-            '           <a href="byui.edu/career/students/job-market-readiness">byui.edu/career/students/job-market-readiness</a>'+
-            '        </p>'+
-            '    </body>'+
-            '</html>';
+            '\n           <br>{your_name}<br>'+
+            '\n           Career Success Mentor<br>'+
+            '\n           BYU-I Career Center | MC 200 <br>'+ 
+            '\n           208.496.9825  <br>'+
+            '\n           <a href="byui.edu/career/students/job-market-readiness">byui.edu/career/students/job-market-readiness</a>'+
+            '\n        </p>'+
+            '\n    </body>'+
+            '\n</html>';
+
+    //change newline to <br> tags
+    html = html.replaceAll('\n', '<br/>');
+
+    fs.writeFile("./templates/"+title+".html", html, (err) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("File written successfully\n");
+          document.getElementById('template_title').innerHTML=''
+          document.getElementById('template_text').innerHTML=''
+          fillTemplateCards()
+        }
+    });
+}
+
+function sendEmail(){
+    email = 'amsb99@gmail.com'
+    body = document.getElementById('template_cards_container').value
+    
+    window.open('mailto:'+email+'?subject="subject"&body='+body)
 }
