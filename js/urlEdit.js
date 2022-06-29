ee69c909246948df642fd71a600ca3315bb52764
michhendrick <104522525+michhendrick@users.noreply.github.com>
2022-06-29 07:46:22 -0600

    Position link editing modal

--- js/urlEdit.js
+++ js/urlEdit.js
@@ -6,11 +6,22 @@
 //   });
 // });
 
+const { get } = require("request-promise");
+
 //document.addEventListener("click", checkIfUrl(event.target))
+
+function getOffset(ele) {
+  const rect = ele.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
+}
 
 function checkIfUrl(value) {
   // Check is the value that was clicked
   var check = value.target;
+  var offsets = getOffset(check);
 
   // If the element you clicked is a link
   if (check.getAttribute("href") !== null) {
@@ -18,16 +29,26 @@ function checkIfUrl(value) {
     loadHref(
       check.textContent,
       check.getAttribute("href"),
-      check.getAttribute("id")
+      check.getAttribute("id"),
+      offsets.left,
+      offsets.top
     );
   }
+
+  // If the modal is already open
+  else if (document.getElementById("urlpopup").style.display == "block"){
+    document.getElementById("urlpopup").style.display = "none";
+  }
+
 }
 
 //fill in the popup with the current values from the url
-function loadHref(text, url, id) {
+function loadHref(text, url, id, x, y) {
   document.querySelector("#urltext").value = text;
   document.querySelector("#urllink").value = url;
   document.querySelector("#id").value = id;
+  document.getElementById("urlpopup").style.left = (x + 10).toString() + "px";
+  document.getElementById("urlpopup").style.top = (y + 30).toString() + "px";
   document.getElementById("urlpopup").style.display = "block";
 }
 
