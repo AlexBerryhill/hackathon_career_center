ab3f50d19040a8ff03c65b24ba849027b6e1d928
Aiden-Patterson <pat18001@byui.edu>
2022-06-28 23:00:01 -0600

    added css for popup

--- js/urlEdit.js
+++ js/urlEdit.js
@@ -1,4 +1,3 @@
-
 // // create an event listener for each url
 // const elements = document.querySelectorAll('a[href]')
 // elements.forEach(element => {
@@ -9,38 +8,36 @@
 
 //document.addEventListener("click", checkIfUrl(event.target))
 
-function checkIfUrl(value){
+function checkIfUrl(value) {
+  // Check is the value that was clicked
+  var check = value.target;
 
-    // Check is the value that was clicked
-    var check = value.target;
-
-    // If the element you clicked is a link
-    if(check.getAttribute('href') !== null){
-
-        // Open the modal for that link
-        loadHref(check.textContent, 
-        check.getAttribute('href'), 
-        check.getAttribute('id'));
-    }
+  // If the element you clicked is a link
+  if (check.getAttribute("href") !== null) {
+    // Open the modal for that link
+    loadHref(
+      check.textContent,
+      check.getAttribute("href"),
+      check.getAttribute("id")
+    );
+  }
 }
 
 //fill in the popup with the current values from the url
-function loadHref(text, url, id){
-document.querySelector("#urltext").value = text;
-document.querySelector("#urllink").value = url;
-document.querySelector("#id").value = id;
-document.getElementById("urlpopup").style.display='block';
+function loadHref(text, url, id) {
+  document.querySelector("#urltext").value = text;
+  document.querySelector("#urllink").value = url;
+  document.querySelector("#id").value = id;
+  document.getElementById("urlpopup").style.display = "block";
 }
 
 // A function that will change the content of the selected tag
-function editHref(form){
+function editHref(form) {
+  // Update the inner html and href of the link, rehide the modal
+  document.getElementById(form.id.value).textContent = form.urltext.value;
+  document.getElementById(form.id.value).href = form.urllink.value;
+  document.getElementById("urlpopup").style.display = "none";
 
-    // Update the inner html and href of the link, rehide the modal
-    document.getElementById(form.id.value).textContent = form.urltext.value;
-    document.getElementById(form.id.value).href = form.urllink.value;
-    document.getElementById("urlpopup").style.display='none';
-    
-    // Return true for.. validation purposes?
-    return true;
+  // Return true for.. validation purposes?
+  return true;
 }
-
